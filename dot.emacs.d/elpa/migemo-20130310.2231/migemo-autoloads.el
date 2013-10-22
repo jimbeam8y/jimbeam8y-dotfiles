0a695e2e0559f26d6c1c225e36c3d3d7790455ff
kawano <kawano@yoshidumi.co.jp>
2013-10-22 16:00:56 +0900

    不要なので削除

--- dot.emacs.d/elpa/migemo-20130310.2231/migemo-autoloads.el
+++
@@ -1,13 +0,0 @@
-;;; migemo-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-(provide 'migemo-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; migemo-autoloads.el ends here
